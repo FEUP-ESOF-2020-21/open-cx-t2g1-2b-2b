050a9eff927b0e31eaf42a0a2feff4d187079835
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-10-30 13:51:18 +0000

    Leaflet maps implementation
    Using: https://github.com/fleaflet/flutter_map

--- src/conf_world/lib/main.dart
+++ src/conf_world/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -26,7 +28,8 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  MyHomePageState createState() => MyHomePageState();
+  MyMapState createState() => MyMapState();
+  //MyHomePageState createState() => MyHomePageState();
 }
 
 class MyHomePageState extends State<MyHomePage> {
@@ -67,5 +70,37 @@ class MyButton extends StatelessWidget {
       ),
       alignment: Alignment.center,
     );
+  }
+}
+
+class MyMapState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('Conf World')),
+        body: new FlutterMap(
+            options: new MapOptions(
+                center: new LatLng(40.71, -74.00), minZoom: 50.0),
+            layers: [
+              new TileLayerOptions(
+                  urlTemplate:"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c']),
+              new MarkerLayerOptions(markers: [
+                new Marker(
+                    width: 45.0,
+                    height: 45.0,
+                    point: new LatLng(40.73, -74.00),
+                    builder: (context) => new Container(
+                      child: IconButton(
+                        icon: Icon(Icons.location_on),
+                        color: Colors.red,
+                        iconSize: 45.0,
+                        onPressed: () {
+                          print('Marker tapped');
+                        },
+                      ),
+                    ))
+              ])
+            ]));
   }
 }
