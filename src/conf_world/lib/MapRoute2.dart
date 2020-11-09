337d05a784d7eebf7c311ee59d420585b8e3374a
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-11-09 19:06:00 +0000

    extract class add

--- src/conf_world/lib/MapRoute2.dart
+++ src/conf_world/lib/MapRoute2.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 import 'package:flutter/widgets.dart';
-import 'MyButton.dart';
 import 'SearchConference.dart';
+import 'MapRoute.dart';
 
 class MapRoute2 extends StatefulWidget {
   @override
@@ -16,51 +16,7 @@ class _MapRoute2State extends State<MapRoute2> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    new FlutterMap(
-      options: new MapOptions(
-        center: new LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        new TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
-          markers: [
-            new Marker(
-              anchorPos: AnchorPos.align(AnchorAlign.top),
-              width: 200.0,
-              height: 50.0,
-              point: new LatLng(51.5, -0.09),
-              builder: (ctx) => new ConferenceIcon(
-                name: 'Computer Science Conference',
-              ),
-            ),
-            new Marker(
-              anchorPos: AnchorPos.align(AnchorAlign.top),
-              width: 200.0,
-              height: 50.0,
-              point: new LatLng(51.53, -0.11),
-              builder: (ctx) => new ConferenceIcon(
-                name: 'Applied Mathematics and Physics',
-              ),
-            ),
-            new Marker(
-              anchorPos: AnchorPos.align(AnchorAlign.top),
-              width: 200.0,
-              height: 50.0,
-              point: new LatLng(51.48, -0.08),
-              builder: (ctx) => new ConferenceIcon(
-                name: 'Robotics and AI',
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
-    SearchConference()
-  ];
+  static List<Widget> _widgetOptions = <Widget>[MapRoute(), SearchConference()];
 
   void _onItemTapped(int index) {
     setState(() {
