2454de3a761fe225570103da06476e652c4081c2
Calmsky <48392665+Calmskyy@users.noreply.github.com>
2020-11-19 18:29:06 +0000

    Add simple world map opening test

---
+++ src/conf_world/test/home_page_test.dart
@@ -0,0 +1,29 @@
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:conf_world/view/home_page.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+void main() {
+
+  testWidgets('World map opens properly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: HomePageState(),
+    ));
+
+    // Verify that we are on the home page.
+    expect(find.text('See World Map'), findsOneWidget);
+
+    // Tap the See World Map option to open the world map.
+    await tester.tap(find.ancestor(of: find.text('See World Map'), matching: find.byType(FlatButton)));
+    await tester.pumpAndSettle();
+
+    // Verify that we are on the world map page.
+    expect(find.byType(FlutterMap), findsOneWidget);
+  });
+}
