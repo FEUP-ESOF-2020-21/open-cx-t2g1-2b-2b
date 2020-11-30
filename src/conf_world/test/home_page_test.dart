309a969808670765c1420833dbb7bac01589463f
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-11-30 18:33:35 +0000

    some important changes

--- src/conf_world/test/home_page_test.dart
+++ src/conf_world/test/home_page_test.dart
@@ -3,6 +3,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:conf_world/view/filter_conference.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:conf_world/view/home_page.dart';
@@ -26,4 +27,15 @@ void main() {
     // Verify that we are on the world map page.
     expect(find.byType(FlutterMap), findsOneWidget);
   });
+
+  testWidgets('Filter Conferences by type test', (WidgetTester tester) async {
+    
+    await tester.pumpWidget(SearchConference());
+
+    //expect(find.text('Type'), findsOneWidget);
+
+    //await tester.tap(find.byType(FlatButton));
+
+  });
+
 }
