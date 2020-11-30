3a013a7933bdb1917b97ef31991cedd81952b3bb
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-11-30 23:00:33 +0000

    Changed icons color and navigation bar color

--- src/conf_world/lib/main.dart
+++ src/conf_world/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:conf_world/database.dart';
 import 'package:conf_world/view/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 
@@ -8,6 +9,14 @@ import 'package:flutter/widgets.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   DatabaseHelper db = new DatabaseHelper();
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xff5a189a),
+    systemNavigationBarDividerColor: Color(0xff5a189a),
+
+    statusBarIconBrightness: Brightness.light,
+  ));
+
   runApp(MaterialApp(
     home: HomePageState(),
   ));
--- src/conf_world/lib/view/navigation.dart
+++ src/conf_world/lib/view/navigation.dart
@@ -30,28 +30,39 @@ class _NavigationState extends State<Navigation> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-          title: new Text('World Map'), backgroundColor: Color(0xff5a189a)),
+          title: new Text('World Map'),
+          backgroundColor: Color(0xff5a189a)
+      ),
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(
-                Icons.home,
-                color: Color(0xffe0aaff),
+              Icons.home,
+              color: Color(0xffe0aaff),
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              color: Color(0xfff3dfff),
             ),
             label: 'Map',
           ),
           BottomNavigationBarItem(
             icon: Icon(
-                Icons.search,
-                color: Color(0xffe0aaff),
+              Icons.search,
+              color: Color(0xffe0aaff),
+            ),
+            activeIcon: Icon(
+              Icons.search,
+              color: Color(0xfff3dfff),
             ),
             label: 'Filter Conferences',
           ),
         ],
         backgroundColor: Color(0xff5a189a),
         currentIndex: _selectedIndex,
-        selectedItemColor: Color(0xffe0aaff),
+        selectedItemColor: Color(0xfff3dfff),
+        unselectedItemColor: Color(0xffe0aaff),
         onTap: _onItemTapped,
       ),
     );
