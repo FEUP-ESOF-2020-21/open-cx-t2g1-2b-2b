399300b66390ca7db96e42072d5373de8bd5eec8
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-12-08 01:53:13 +0000

    First version of date range picker

--- src/conf_world/lib/view/screens/filter_date.dart
+++ src/conf_world/lib/view/screens/filter_date.dart
@@ -3,18 +3,55 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-class FilterByDate extends StatelessWidget {
+class FilterByDate extends StatefulWidget {
   final FilterDateModel model = FilterDateModel();
+
+  @override
+  FilterByDateState createState() => FilterByDateState();
+}
+
+class FilterByDateState extends State<FilterByDate> {
+  DateTimeRange _date = DateTimeRange(
+    start: DateTime(2020, 11, 17),
+    end: DateTime(2020, 11, 24),
+  );
+
+  void _selectDate() async {
+    final DateTimeRange newDate = await showDateRangePicker(
+      context: context,
+      initialDateRange: _date,
+      firstDate: DateTime(2017, 1),
+      lastDate: DateTime(2022, 7),
+      helpText: 'Select a date range',
+    );
+    if (newDate != null) {
+      setState(() {
+        _date = newDate;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: model.backgroundColor,
-        appBar: AppBar(
-          title: Text("Filter By Date"),
-          centerTitle: true,
-          backgroundColor: model.appBarBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _selectDate,
+              child: Text('SELECT DATE RANGE'),
+            ),
+            SizedBox(height: 8),
+            Center(
+              child: Text(
+                'Selected date range: \n$_date',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
